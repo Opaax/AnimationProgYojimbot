5be3e3d3caa2b0984171e45369e46a17cd2ebcbc
Enguerran <61538252+Opaax@users.noreply.github.com>
2023-03-01 17:36:48 +0100

    Reset All

--- Source/Yojimbot_UE/Yojimbot_UE.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Yojimbot_UE.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Yojimbot_UE, "Yojimbot_UE" );
- 
